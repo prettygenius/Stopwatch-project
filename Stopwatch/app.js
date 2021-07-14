dfbc97a5d630ec33ed477852f6cf9cb561af93af
prettygenius <64159001+prettygenius@users.noreply.github.com>
2021-07-13 23:03:41 -0500

    Add files via upload

---
+++ Stopwatch/app.js
@@ -0,0 +1,11 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+const counter = document.getElementById('counter');
+
+
+start.addEventListener('click', function(){
+
+   counter.setMilliseconds()
+})
+    
